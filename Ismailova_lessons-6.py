080ea61625a1b0495a75090124d1f5773aac03da
Sabina <sabinaismailovam@gmail.com>
2022-02-08 09:29:07 +0200

    les-6 (part)

---
+++ Ismailova_lessons-6.py
@@ -0,0 +1,56 @@
+# #Задание 1
+# from time import sleep
+#
+# class TrafficLight:
+#     __color = ['red', 'yelloy', 'green']
+#     def turn_on(self):
+#         i = 0
+#         while i < 3:
+#             print(f'{TrafficLight.__color[i]}', 'Ожидание...')
+#             if i == 0:
+#                 sleep(7)
+#             elif i == 1:
+#                 sleep(5)
+#             elif i == 2:
+#                 sleep(3)
+#             i += 1
+#
+# TrafficLight = TrafficLight()
+# TrafficLight.turn_on()
+
+# #Задание  2
+#
+# class Road:
+#     __length = None
+#     __width = None
+#     mass_m2 = None
+#     thickness = None
+#
+#     def __init__(self, length, width):
+#         self.length = length
+#         self.width = width
+#
+#     def pokrytie(self):
+#         self.mass_m2 = 25
+#         self.thickness = 0.05
+#         pokrytie = (self.length * self.width * self.mass_m2 * self.thickness)//1000
+#         print(f'Необходимо {pokrytie} тонн для покрытия всей дороги')
+#
+# road = Road(20, 5000)
+# road.pokrytie()
+
+#Задание 3
+
+    #     def mass(self):
+    #     mass =_ength * _width * _mass_m2 * _thickness
+    #     print(mass)
+    #
+    # _ength = int(input(''))
+    # _width = int(input(''))
+    # _thickness = int(input(''))
+    #
+    #
+    #
+    #
+    #
+
